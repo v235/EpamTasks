8d9ec118bcc4173796bda2eb215df4e70f12291b
Vitaly Pimenau <vi2aly235@gmail.com>
2017-04-11 23:57:45 +0300

    add Serialization

---
+++ 09-Serialization/Serialization.Tasks/Company.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Serialization.Tasks
+{
+    // TODO : Make Company class xml-serializable using DataContractSerializer 
+    // Employee.Manager should be serialized as reference
+    // Company class has to be forward compatible with all derived versions
+
+
+    [DataContract]
+    public class Company : IExtensibleDataObject
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public IList<Employee> Employee { get; set; }
+
+        public virtual ExtensionDataObject ExtensionData { get; set; }
+    }
+
+    [DataContract(IsReference = true)]
+    [KnownType(typeof (Worker))]
+    [KnownType(typeof (Manager))]
+    public abstract class Employee
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string LastName { get; set; }
+
+        [DataMember]
+        public string Title { get; set; }
+
+        [DataMember]
+        public Manager Manager { get; set; }
+    }
+
+    [DataContract]
+    public class Worker : Employee
+    {
+        public int Salary { get; set; }
+    }
+
+    [DataContract(IsReference = true)]
+    public class Manager : Employee
+    {
+        public int YearBonusRate { get; set; }
+    }
+
+}
---
+++ 09-Serialization/Serialization.Tasks/GoogleSearchResult.cs
@@ -0,0 +1,72 @@
+﻿
+using System.Runtime.Remoting.Contexts;
+using System.Runtime.Serialization;
+
+namespace Serialization.Tasks
+{
+
+    // TODO: Implement GoogleSearchResult class to be deserialized from Google Search API response
+    // Specification is available at: https://developers.google.com/custom-search/v1/using_rest#WorkingResults
+    // The test json file is at Serialization.Tests\Resources\GoogleSearchJson.txt
+
+
+    [DataContract]
+    public class GoogleSearchResult
+    {
+        [DataMember(Name = "kind")] public string Kind;
+        [DataMember(Name = "url")] public Url Url;
+        [DataMember(Name = "queries")] public Queries Queries;
+        [DataMember(Name = "context")] public Context Context;
+        [DataMember(Name = "items")] public Item[] Items;
+    }
+
+    [DataContract]
+    public class Url
+    {
+        [DataMember(Name = "type")] public string Type;
+        [DataMember(Name = "template")] public string Template;
+    }
+
+    [DataContract]
+    public class Page
+    {
+        [DataMember(Name = "title")] public string Title;
+        [DataMember(Name = "totalResults")] public long TotalResults;
+        [DataMember(Name = "searchTerms")] public string SearchTerms;
+        [DataMember(Name = "count")] public int Count;
+        [DataMember(Name = "startIndex")] public int StartIndex;
+        [DataMember(Name = "inputEncoding")] public string InputEncoding;
+        [DataMember(Name = "outputEncoding")] public string OutputEncoding;
+        [DataMember(Name = "cx")] public string Cx;
+    }
+
+    [DataContract]
+    public class Queries
+    {
+        [DataMember(Name = "previousPage")] public Page[] PreviousPage;
+        [DataMember(Name = "nextPage")] public Page[] NextPage;
+        [DataMember(Name = "request")] public Page[] Request;
+    }
+
+    [DataContract]
+    public class Context
+    {
+        [DataMember(Name = "title")] public string Title;
+    }
+
+    [DataContract]
+    public class Item
+    {
+        [DataMember(Name = "kind")] public string Kind;
+        [DataMember(Name = "title")] public string Title;
+        [DataMember(Name = "htmlTitle")] public string HtmlTitle;
+        [DataMember(Name = "link")] public string Link;
+        [DataMember(Name = "displayLink")] public string DisplayLink;
+        [DataMember(Name = "snippet")] public string Snippet;
+        [DataMember(Name = "htmlSnippet")] public string HtmlSnippet;
+    }
+
+}
+
+
+
